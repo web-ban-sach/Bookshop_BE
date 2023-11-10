c97e379ced18a38e733439a27d02f87ba80663c6
CuongBui157 <cuongbui1572003@gmail.com>
2023-11-10 13:29:16 +0700

    fixed bug in authenticateToken.js

--- src/middlewares/authenticateToken.js
+++ src/middlewares/authenticateToken.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 
 export const authenticateToken = (req, res, next) => {
-    const token = req.headers.authorization
+    const token = req.headers.authorization.split(' ')[1]
 
     if (!token) {
         return res.status(401).json({ message: 'Không tìm thấy token xác thực' })
